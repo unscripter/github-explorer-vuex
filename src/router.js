9e767d5b2e4c23ae0ce016f399dc2720842857e7
Amit Mundra <amit.mundra52@gmail.com>
2018-07-09 19:29:32 +0530

    routing created for using vue-router

---
+++ src/router.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ProfileView from './views/ProfileView.vue'
+import About from './views/About.vue'
+import App from './App'
+// import RepoList from '@/components/RepoList'
+// import RepoDetail from '@/components/RepoDetails'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'App',
+      component: ProfileView,
+      children: [
+          {
+              path: 'user/:username',
+              name: 'USER_DETAIL',
+              component: ProfileView
+          },
+          {
+              path: '*',
+              redirect: {
+                  name: 'App',
+                  params: {
+                      username: 'AmitMundra54'
+                  }
+              }
+          }
+      ]
+  },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    }
+  ]
+})
