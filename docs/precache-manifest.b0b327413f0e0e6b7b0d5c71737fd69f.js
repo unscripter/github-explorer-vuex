08c93941ff6232fed06d031d58f2e8e22cdcafeb
Amit Mundra <amit.mundra52@gmail.com>
2018-07-17 21:24:40 +0530

    first build created in docs

---
+++ docs/precache-manifest.b0b327413f0e0e6b7b0d5c71737fd69f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74db2300deeab02c22b6",
+    "url": "/js/chunk-vendors.74db2300.js"
+  },
+  {
+    "revision": "fd94e0520f8dc34cfd7b",
+    "url": "/js/app.fd94e052.js"
+  },
+  {
+    "revision": "102f1021b6cbf7aa6439",
+    "url": "/js/0.102f1021.js"
+  },
+  {
+    "revision": "594712f3a6714b1489655a08352b3891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd94e0520f8dc34cfd7b",
+    "url": "/css/app.0718e453.css"
+  },
+  {
+    "revision": "102f1021b6cbf7aa6439",
+    "url": "/css/0.f5941131.css"
+  }
+];
