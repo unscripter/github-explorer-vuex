acec6e1c4726e8d160525f558c9cd19f6bed604f
Amit Mundra <amit.mundra52@gmail.com>
2018-07-17 21:23:11 +0530

    store updated

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import modules from './modules';
+import createLogger from 'vuex/dist/logger';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules
+    modules,
+    plugins: [createLogger()]
 });
